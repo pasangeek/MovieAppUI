7cdcef5df2ca36a9f56f1943f09b3641e37e9ad3
pasangeek <pasan.madusanka9@gmail.com>
2024-07-13 22:24:39 +0900

    add app route navigation to main class

--- app/src/main/java/com/example/movieappui/MainActivity.kt
+++ app/src/main/java/com/example/movieappui/MainActivity.kt
@@ -3,16 +3,9 @@ package com.example.movieappui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.example.movieappui.ui.route.AppRoute
 import com.example.movieappui.ui.theme.MovieAppUITheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -34,6 +27,7 @@ class MainActivity : ComponentActivity() {
             MovieAppUITheme {
                 /// main navigation
                 val navController = rememberNavController()
+                AppRoute.GenerateRoute(navController = navController)
             }
         }
     }
