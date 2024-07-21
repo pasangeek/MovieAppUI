c40859cb48478504f18fa6ec854f631058653655
pasangeek <pasan.madusanka9@gmail.com>
2024-07-21 22:32:59 +0900

    implement detail page

--- app/src/main/java/com/example/movieappui/ui/DetailScreen.kt
+++ app/src/main/java/com/example/movieappui/ui/DetailScreen.kt
@@ -1,12 +1,177 @@
 package com.example.movieappui.ui
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.movieappui.R
 import com.example.movieappui.model.MovieModel
+import com.example.movieappui.route.RouteName
+import com.example.movieappui.ui.theme.Gray
+import com.example.movieappui.ui.theme.Yellow
 
 @Composable
 fun DetailScreen(
     navController: NavHostController,
     movie: MovieModel,
 ) {
+    val scrollState = rememberScrollState()
+    Scaffold(
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            Button(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .height(56.dp),
+                colors = ButtonDefaults.buttonColors(
+                     Yellow
+                ),
+                shape = RoundedCornerShape(32.dp),
+                onClick = {
+                    navController.navigate(RouteName.SeatSelector)
+                },
+            ) {
+                Text(text = "Booking Now")
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+        ) {
+            Row(
+                modifier = Modifier.padding(
+                    horizontal = 16.dp, vertical = 8.dp
+                ),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "Back Button")
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(text = "Movie Detail", style = MaterialTheme.typography.headlineSmall)
+            }
+            Row(
+                modifier = Modifier
+                    .height(320.dp)
+                    .padding(horizontal = 24.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = movie.assetImage),
+                    contentDescription = "Movie Image",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .weight(0.7f)
+                        .height(320.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
+                Spacer(modifier = Modifier.width(24.dp))
+                Column(
+                    modifier = Modifier
+                        .height(320.dp)
+                        .weight(0.3f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    MovieInfo(
+                        painterResourceId = R.drawable.baseline_videocam,
+                        title = "Genre",
+                        value = movie.type
+                    )
+                    MovieInfo(
+                        painterResourceId = R.drawable.baseline_access_time_filled,
+                        title = "Duration",
+                        value = movie.duration
+                    )
+                    MovieInfo(
+                        painterResourceId = R.drawable.baseline_stars,
+                        title = "Rating",
+                        value = movie.rating
+                    )
+                }
+            }
+            Text(
+                movie.title, style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.padding(
+                    horizontal = 24.dp, vertical = 16.dp
+                )
+            )
+            Text(
+                "Synopsis", style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(
+                    horizontal = 24.dp
+                )
+            )
+            Text(
+                movie.synopsis, style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(
+                    horizontal = 24.dp, vertical = 16.dp
+                )
+            )
+            Spacer(modifier = Modifier.height(64.dp))
+        }
+    }
+}
+@Composable
+fun MovieInfo(
+    @DrawableRes painterResourceId: Int,
+    title: String,
+    value: String,
+) {
+    Column(
+        modifier = Modifier
+            .border(width = 1.dp, color = Gray, shape = RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(16.dp))
+            .clickable { }
+            .padding(12.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Icon(
+            painter = painterResource(id = painterResourceId),
+            contentDescription = title,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = title, style = MaterialTheme.typography.bodyMedium)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = value, style = MaterialTheme.typography.titleSmall)
+    }
 }
